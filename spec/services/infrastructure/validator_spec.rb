2c35997c300231b638b7854e5bc8e0a4973a0516
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-23 20:04:25 -0300

    test: ensure Validator throws ArgumentError if param is not Integer, DateTime or BigDecimal

--- spec/services/infrastructure/validator_spec.rb
+++ spec/services/infrastructure/validator_spec.rb
@@ -14,5 +14,23 @@ describe 'Validator' do
         Validator.assert_object_class_type ''
       }.to raise_error(ArgumentError)
     end
+
+    it 'Is not Integer' do
+      expect{
+        Validator.assert_integer_number 3.0
+      }.to raise_error(ArgumentError)
+    end
+
+    it 'Is not BigDecimal' do
+      expect{
+        Validator.assert_big_decimal 3.0
+      }.to raise_error(ArgumentError)
+    end
+
+    it 'Is not DateTime' do
+      expect{
+        Validator.assert_datetime "2022-04-23T19:59:55-03:00"
+      }.to raise_error(ArgumentError)
+    end
   end
 end
