9e6a19c4102c3c261706ff0dc487b6691e05cbd7
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-23 17:59:04 -0300

    feat: ensure Deputy model returns valid deputy object

---
+++ app/models/deputy.rb
@@ -0,0 +1,28 @@
+require './app/services/infrastructure/validator.rb'
+
+class Deputy < ApplicationRecord
+  def custom_initialize(name:, ide_register:, current_uf:)
+    change_name(name: name)
+    change_ide_register(ide_register: ide_register)
+    change_current_uf(current_uf: current_uf)
+  end
+
+  private
+  def change_name(name:)
+    Validator.assert_empty_string name
+    self.name = name.upcase
+    self
+  end
+
+  def change_ide_register(ide_register:)
+    Validator.assert_empty_string ide_register
+    self.ide_register = ide_register
+    self
+  end
+
+  def change_current_uf(current_uf:)
+    Validator.assert_empty_string current_uf
+    self.current_uf = current_uf.upcase
+    self
+  end
+end
