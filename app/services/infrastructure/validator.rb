3c3c83d1ba57b0128601dd02a661bc5414dd7f6a
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-23 20:04:08 -0300

    feat: ensure Validator throws ArgumentError if param is not Integer, DateTime or BigDecimal

--- app/services/infrastructure/validator.rb
+++ app/services/infrastructure/validator.rb
@@ -4,6 +4,18 @@ class Validator
     raise ArgumentError.new "Empty string" if string.empty?
   end
 
+  def self.assert_integer_number number 
+    self.assert_object_class_type number, Integer
+  end
+
+  def self.assert_datetime datetime 
+    self.assert_object_class_type datetime, DateTime
+  end
+
+  def self.assert_big_decimal big_decimal 
+    self.assert_object_class_type big_decimal, BigDecimal
+  end
+
   def self.assert_object_class_type object, class_type
     raise ArgumentError.new "Object is not #{class_type} class" unless object.instance_of? class_type
   end
