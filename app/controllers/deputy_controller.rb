ab7d500e2c70e1cb027409ea17b94e24fe0c8ac8
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-24 15:27:52 -0300

    feat: ensure ExtractExpenseDataFromCsvService receives file and uf by params in DeputyController

--- app/controllers/deputy_controller.rb
+++ app/controllers/deputy_controller.rb
@@ -1,2 +1,7 @@
+require './app/services/deputy_management/register_deputies_expenses/extract_expense_data_from_csv_service.rb'
+
 class DeputyController < ApplicationController
+  def submit_csv
+    expense_data = ExtractExpenseDataFromCsvService.new(file: params[:file], uf: params[:uf]).call
+  end
 end
