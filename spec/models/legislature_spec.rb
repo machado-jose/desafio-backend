cec703afec92a2a91758432e46be9842ffb90703
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-24 11:01:09 -0300

    test: ensure Legislature model adds deputy expense

--- spec/models/legislature_spec.rb
+++ spec/models/legislature_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Legislature' do
-  context 'initialize' do
+  context '#initialize' do
     before :each do
       @deputy = create(:deputy) 
       @legislature_number = Faker::Code.unique.npi 
@@ -137,6 +137,140 @@ describe 'Legislature' do
             party_acronym: @party_acronym,
             legislature_code: nil
           ).valid?
+        }.to raise_error(ActiveModel::StrictValidationFailed)
+      end
+    end
+  end
+
+  context '#add_deputy_expense' do
+    before :each do 
+      @legislature = create(:legislature) 
+      @sub_quota_number = Faker::Code.unique.npi 
+      @sub_quota_description = "any_sub_quota_description" 
+      @sub_quota_specification_number = Faker::Code.unique.npi 
+      @sub_quota_specification_description = "any_sub_quota_specification_description" 
+      @provider = Faker::Name.name 
+      @provider_registration_number = "999.999.999-99" 
+      @document_number = Faker::Code.unique.npi 
+      @document_type = 0
+      @issuance_date = DateTime.now - 3.months 
+      @document_value = 300.to_d 
+      @reverse_value = 100.to_d 
+      @net_value = 200.to_d 
+      @competence = (DateTime.now - 3.months).strftime("%Y-%m") 
+      @installment_number = 1 
+      @passager = Faker::Name.name 
+      @leg_trip = "any_leg_trip" 
+      @batch_number = Faker::Code.unique.npi 
+      @reimbursement_number = Faker::Code.unique.npi 
+      @refund_value = 200.to_d 
+      @applicant_identifier = Faker::Code.unique.npi
+    end
+
+    it 'Should add new deputy expense' do
+      expect{
+        @legislature.add_deputy_expense(
+          sub_quota_number: @sub_quota_number, 
+          sub_quota_description: @sub_quota_description, 
+          sub_quota_specification_number: @sub_quota_specification_number, 
+          sub_quota_specification_description: @sub_quota_specification_description, 
+          provider: @provider, 
+          provider_registration_number: @provider_registration_number, 
+          document_number: @document_number,
+          document_type: @document_type, 
+          issuance_date: @issuance_date, 
+          document_value: @document_value, 
+          reverse_value: @reverse_value, 
+          net_value: @net_value, 
+          competence: @competence,
+          installment_number: @installment_number, 
+          passager: @passager, 
+          leg_trip: @leg_trip, 
+          batch_number: @batch_number, 
+          reimbursement_number: @reimbursement_number, 
+          refund_value: @refund_value, 
+          applicant_identifier: @applicant_identifier
+        )
+        @legislature.save!
+      }.to change { FinancialManagement::DeputyExpense.count }.by(1)
+    end
+
+    it 'Should not save deputy expense with duplicate document number' do
+      @legislature.add_deputy_expense(
+        sub_quota_number: @sub_quota_number, 
+        sub_quota_description: @sub_quota_description, 
+        sub_quota_specification_number: @sub_quota_specification_number, 
+        sub_quota_specification_description: @sub_quota_specification_description, 
+        provider: @provider, 
+        provider_registration_number: @provider_registration_number, 
+        document_number: @document_number,
+        document_type: @document_type, 
+        issuance_date: @issuance_date, 
+        document_value: @document_value, 
+        reverse_value: @reverse_value, 
+        net_value: @net_value, 
+        competence: @competence,
+        installment_number: @installment_number, 
+        passager: @passager, 
+        leg_trip: @leg_trip, 
+        batch_number: @batch_number, 
+        reimbursement_number: @reimbursement_number, 
+        refund_value: @refund_value, 
+        applicant_identifier: @applicant_identifier
+      )
+      @legislature.save!
+      expect{
+        @legislature.add_deputy_expense(
+          sub_quota_number: @sub_quota_number, 
+          sub_quota_description: @sub_quota_description, 
+          sub_quota_specification_number: @sub_quota_specification_number, 
+          sub_quota_specification_description: @sub_quota_specification_description, 
+          provider: @provider, 
+          provider_registration_number: @provider_registration_number, 
+          document_number: @document_number,
+          document_type: @document_type, 
+          issuance_date: @issuance_date, 
+          document_value: @document_value, 
+          reverse_value: @reverse_value, 
+          net_value: @net_value, 
+          competence: @competence,
+          installment_number: @installment_number, 
+          passager: @passager, 
+          leg_trip: @leg_trip, 
+          batch_number: @batch_number, 
+          reimbursement_number: @reimbursement_number, 
+          refund_value: @refund_value, 
+          applicant_identifier: @applicant_identifier
+        )
+        @legislature.save!
+      }.to change { FinancialManagement::DeputyExpense.count }.by(0)
+    end
+
+    context 'Should raise an error' do
+      it 'when legislature is invalid' do
+        expect{
+          @legislature.add_deputy_expense(
+            sub_quota_number: @sub_quota_number, 
+            sub_quota_description: @sub_quota_description, 
+            sub_quota_specification_number: @sub_quota_specification_number, 
+            sub_quota_specification_description: @sub_quota_specification_description, 
+            provider: @provider, 
+            provider_registration_number: @provider_registration_number, 
+            document_number: Faker::Code.unique.npi,
+            document_type: @document_type, 
+            issuance_date: @issuance_date, 
+            document_value: @document_value, 
+            reverse_value: @reverse_value, 
+            net_value: nil, 
+            competence: @competence,
+            installment_number: @installment_number, 
+            passager: @passager, 
+            leg_trip: @leg_trip, 
+            batch_number: @batch_number, 
+            reimbursement_number: @reimbursement_number, 
+            refund_value: @refund_value, 
+            applicant_identifier: @applicant_identifier
+          )
         }.to raise_error(ActiveModel::StrictValidationFailed)
       end
     end
