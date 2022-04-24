592a6965131a06b46a055960d430bb3b9d273bdf
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-24 18:55:42 -0300

    refactor: pass params to call method in CreateDeputyService

--- app/services/deputy_management/create_deputy_service.rb
+++ app/services/deputy_management/create_deputy_service.rb
@@ -1,15 +1,9 @@
 class CreateDeputyService
-  def initialize(name:, ide_register:, deputy_wallet_number:)
-    @name = name
-    @ide_register = ide_register
-    @deputy_wallet_number = deputy_wallet_number
-  end
-
-  def call
+  def call(name:, ide_register:, deputy_wallet_number:)
     deputy = Deputy.new(
-      name: @name, 
-      ide_register: @ide_register, 
-      deputy_wallet_number: @deputy_wallet_number
+      name: name, 
+      ide_register: ide_register, 
+      deputy_wallet_number: deputy_wallet_number
     )
     deputy.valid?
     deputy.save!
--- spec/services/deputy_management/create_deputy_service_spec.rb
+++ spec/services/deputy_management/create_deputy_service_spec.rb
@@ -10,11 +10,11 @@ describe 'CreateDeputyService' do
     end
 
     it 'Should create deputy' do
-      deputy = CreateDeputyService.new(
+      deputy = CreateDeputyService.new.call(
                                         name: @name,
                                         ide_register: @ide_register,
                                         deputy_wallet_number: @deputy_wallet_number
-                                      ).call
+                                      )
       expect(deputy.name).to eq(@name.upcase)
       expect(deputy.ide_register).to eq(@ide_register)
       expect(deputy.deputy_wallet_number).to eq(@deputy_wallet_number)
@@ -23,61 +23,61 @@ describe 'CreateDeputyService' do
     context 'Should raise an error' do
       it 'when passing empty name' do
         expect{
-          CreateDeputyService.new(
+          CreateDeputyService.new.call(
                                     name: '',
                                     ide_register: @ide_register,
                                     deputy_wallet_number: @deputy_wallet_number
-                                  ).call
+                                  )
         }.to raise_error(ActiveModel::StrictValidationFailed)
       end
 
       it 'when passing nil name' do
         expect{
-          CreateDeputyService.new(
+          CreateDeputyService.new.call(
                                     name: nil,
                                     ide_register: @ide_register,
                                     deputy_wallet_number: @deputy_wallet_number
-                                  ).call
+                                  )
         }.to raise_error(ActiveModel::StrictValidationFailed)
       end
 
       it 'when passing empty ide_register' do
         expect{
-          CreateDeputyService.new(
+          CreateDeputyService.new.call(
                                     name: @name,
                                     ide_register: '',
                                     deputy_wallet_number: @deputy_wallet_number
-                                  ).call
+                                  )
         }.to raise_error(ActiveModel::StrictValidationFailed)
       end
 
       it 'when passing nil ide_register' do
         expect{
-          CreateDeputyService.new(
+          CreateDeputyService.new.call(
                                     name: @name,
                                     ide_register: nil,
                                     deputy_wallet_number: @deputy_wallet_number
-                                  ).call
+                                  )
         }.to raise_error(ActiveModel::StrictValidationFailed)
       end
 
       it 'when passing empty deputy_wallet_number' do
         expect{
-          CreateDeputyService.new(
+          CreateDeputyService.new.call(
                                     name: @name,
                                     ide_register: @ide_register,
                                     deputy_wallet_number: ''
-                                  ).call
+                                  )
         }.to raise_error(ActiveModel::StrictValidationFailed)
       end
 
       it 'when passing nil deputy_wallet_number' do
         expect{
-          CreateDeputyService.new(
+          CreateDeputyService.new.call(
                                     name: @name,
                                     ide_register: @ide_register,
                                     deputy_wallet_number: nil
-                                  ).call
+                                  )
         }.to raise_error(ActiveModel::StrictValidationFailed)
       end
     end
