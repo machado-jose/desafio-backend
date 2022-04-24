594ee16e8a8a0f86631835b11539e4b9005e5653
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-24 16:09:33 -0300

    test: ensure CreateDeputyService creates valid deputy

---
+++ spec/services/deputy_management/create_deputy_service_spec.rb
@@ -0,0 +1,85 @@
+require 'rails_helper'
+require './app/services/deputy_management/create_deputy_service.rb'
+
+describe 'CreateDeputyService' do
+  context '#call' do
+    before :each do
+      @name = Faker::Name.unique.name 
+      @ide_register = Faker::Code.unique.npi
+      @deputy_wallet_number = Faker::Code.unique.npi 
+    end
+
+    it 'Should create deputy' do
+      deputy = CreateDeputyService.new(
+                                        name: @name,
+                                        ide_register: @ide_register,
+                                        deputy_wallet_number: @deputy_wallet_number
+                                      ).call
+      expect(deputy.name).to eq(@name.upcase)
+      expect(deputy.ide_register).to eq(@ide_register)
+      expect(deputy.deputy_wallet_number).to eq(@deputy_wallet_number)
+    end
+
+    context 'Should raise an error' do
+      it 'when passing empty name' do
+        expect{
+          CreateDeputyService.new(
+                                    name: '',
+                                    ide_register: @ide_register,
+                                    deputy_wallet_number: @deputy_wallet_number
+                                  ).call
+        }.to raise_error(ActiveModel::StrictValidationFailed)
+      end
+
+      it 'when passing nil name' do
+        expect{
+          CreateDeputyService.new(
+                                    name: nil,
+                                    ide_register: @ide_register,
+                                    deputy_wallet_number: @deputy_wallet_number
+                                  ).call
+        }.to raise_error(ActiveModel::StrictValidationFailed)
+      end
+
+      it 'when passing empty ide_register' do
+        expect{
+          CreateDeputyService.new(
+                                    name: @name,
+                                    ide_register: '',
+                                    deputy_wallet_number: @deputy_wallet_number
+                                  ).call
+        }.to raise_error(ActiveModel::StrictValidationFailed)
+      end
+
+      it 'when passing nil ide_register' do
+        expect{
+          CreateDeputyService.new(
+                                    name: @name,
+                                    ide_register: nil,
+                                    deputy_wallet_number: @deputy_wallet_number
+                                  ).call
+        }.to raise_error(ActiveModel::StrictValidationFailed)
+      end
+
+      it 'when passing empty deputy_wallet_number' do
+        expect{
+          CreateDeputyService.new(
+                                    name: @name,
+                                    ide_register: @ide_register,
+                                    deputy_wallet_number: ''
+                                  ).call
+        }.to raise_error(ActiveModel::StrictValidationFailed)
+      end
+
+      it 'when passing nil deputy_wallet_number' do
+        expect{
+          CreateDeputyService.new(
+                                    name: @name,
+                                    ide_register: @ide_register,
+                                    deputy_wallet_number: nil
+                                  ).call
+        }.to raise_error(ActiveModel::StrictValidationFailed)
+      end
+    end
+  end
+end
