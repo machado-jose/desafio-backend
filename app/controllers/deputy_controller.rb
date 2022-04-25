084a4cfb9ee09568c4d23b9e3bc73b2a4d855c41
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-24 21:02:50 -0300

    feat: ensure DeputyController saves deputies, legislatures and deputy expenses by file

--- app/controllers/deputy_controller.rb
+++ app/controllers/deputy_controller.rb
@@ -1,7 +1,15 @@
 require './app/services/deputy_management/register_deputies_expenses/extract_expense_data_from_csv_service.rb'
+require './app/services/deputy_management/register_deputies_expenses/analyze_expense_data_service.rb'
+require './app/services/deputy_management/create_deputy_service.rb'
+require './app/services/deputy_management/register_deputies_expenses/add_expense_service.rb'
 
 class DeputyController < ApplicationController
   def submit_csv
     expense_data = ExtractExpenseDataFromCsvService.new(file: params[:file], uf: params[:uf]).call
+    AnalyzeExpenseDataService.new(
+                                    expense_data: expense_data, 
+                                    create_deputy_service: CreateDeputyService.new, 
+                                    add_expense_service: AddExpenseService.new
+                                  ).call
   end
 end
