48d1c57d485d6a3c5aa54c34aa65f15f0df1a111
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-24 21:03:05 -0300

    test: ensure DeputyController saves deputies, legislatures and deputy expenses by file

--- spec/controllers/deputy_controller_spec.rb
+++ spec/controllers/deputy_controller_spec.rb
@@ -5,14 +5,20 @@ RSpec.describe DeputyController, type: :controller do
   context '#submit_csv' do
     before :each do
       @file = fixture_file_upload('services/register_deputies_expenses/Ano-2021.csv', 'text/csv')
-      allow(ActionDispatch::Http::UploadedFile).to receive(:new).and_return(@file)
     end
 
     it 'Should pass file and uf to ExtractExpenseDataFromCsvService' do
+      allow(ActionDispatch::Http::UploadedFile).to receive(:new).and_return(@file)
       extract_expense_data_from_csv_service = instance_double(ExtractExpenseDataFromCsvService)
       expect(ExtractExpenseDataFromCsvService).to receive(:new).with(file: @file, uf: "RJ").and_return(extract_expense_data_from_csv_service)
       expect(extract_expense_data_from_csv_service).to receive(:call).and_return({})
       post :submit_csv, params: {file: @file, uf: 'RJ'}
+    end
+
+    it 'Should save deputies, legislatures and deputy expenses by file' do
+      expect {
+        post :submit_csv, params: {file: @file}
+      }.to change { FinancialManagement::DeputyExpense.count }.by(2).and change { Legislature.count }.by(2).and change { Deputy.count }.by(2) 
     end
   end
 end
