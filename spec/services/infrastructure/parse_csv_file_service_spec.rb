69489db2604deb43ecfaa20f9c3fe5cc5e4e2a3f
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-26 10:14:33 -0300

    test: ensure ParseCsvFileService removes comma row values

--- spec/services/infrastructure/parse_csv_file_service_spec.rb
+++ spec/services/infrastructure/parse_csv_file_service_spec.rb
@@ -14,5 +14,12 @@ describe 'ParseCsvFileService' do
       csv = ParseCsvFileService.new(file: file).call
       expect(csv.headers.count).to eq(31)
     end
+
+    it 'Should remove comma row values' do
+      file = fixture_file_upload('services/register_deputies_expenses/mal_formatted_2.csv', 'text/csv')
+      csv = ParseCsvFileService.new(file: file).call
+      expect(csv.headers.count).to eq(31)
+      expect(csv.by_row[0]['txtCNPJCPF']).to eq("093.270.360/0015-9")
+    end
   end
 end
