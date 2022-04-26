d2c76cc900d582a3ecdb3826b4094fd6a26440d7
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-26 08:48:20 -0300

    refactor: ensure ParseCsvFileService avoid loop when raise CSV::MalformedCSVError twice

--- app/services/infrastructure/parse_csv_file_service.rb
+++ app/services/infrastructure/parse_csv_file_service.rb
@@ -6,11 +6,17 @@ class ParseCsvFileService
   end
 
   def call
+    retry_twice = false
     begin
       csv = CSV.parse(@file, headers: true)
-    rescue CSV::MalformedCSVError => e  
-      treat_csv_file
-      retry
+    rescue CSV::MalformedCSVError => e
+      unless retry_twice
+        retry_twice = true
+        treat_csv_file
+        retry
+      else  
+        raise e
+      end
     end
     csv
   end
