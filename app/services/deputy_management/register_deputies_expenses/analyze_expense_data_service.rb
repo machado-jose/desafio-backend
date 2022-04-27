e481163794d7b3144de09b9ad7cd57c742bf7a12
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-26 21:31:52 -0300

    refactor: remove begin and rescue in AnalyzeExpenseDataService

--- app/services/deputy_management/register_deputies_expenses/analyze_expense_data_service.rb
+++ app/services/deputy_management/register_deputies_expenses/analyze_expense_data_service.rb
@@ -27,41 +27,36 @@ class AnalyzeExpenseDataService
             party_acronym: deputy_expense_data["sgPartido"], 
             legislature_code: deputy_expense_data["codLegislatura"]
           )
-          begin
-            @add_expense_service.call(
-              deputy: deputy, 
-              legislature_code: deputy_expense_data["codLegislatura"], 
-              sub_quota_number: deputy_expense_data["numSubCota"], 
-              sub_quota_description: deputy_expense_data["txtDescricao"], 
-              sub_quota_specification_number: deputy_expense_data["numEspecificacaoSubCota"], 
-              sub_quota_specification_description: deputy_expense_data["txtDescricaoEspecificacao"], 
-              provider: deputy_expense_data["txtFornecedor"], 
-              provider_registration_number: deputy_expense_data["txtCNPJCPF"], 
-              document_number: deputy_expense_data["txtNumero"], 
-              document_type: analyze_document_type(document_type: deputy_expense_data["indTipoDocumento"].to_i ), 
-              issuance_date: deputy_expense_data["datEmissao"], 
-              document_value: deputy_expense_data["vlrDocumento"], 
-              reverse_value: deputy_expense_data["vlrGlosa"], 
-              net_value: deputy_expense_data["vlrLiquido"], 
-              expense_month: deputy_expense_data["numMes"], 
-              expense_year: deputy_expense_data["numAno"], 
-              installment_number: deputy_expense_data["numParcela"], 
-              passager: deputy_expense_data["txtPassageiro"], 
-              leg_trip: deputy_expense_data["txtTrecho"], 
-              batch_number: deputy_expense_data["numLote"], 
-              reimbursement_number: deputy_expense_data["numRessarcimento"], 
-              refund_value: deputy_expense_data["vlrRestituicao"], 
-              applicant_identifier: deputy_expense_data["ideDocumento"],
-              document_url: deputy_expense_data["urlDocumento"]
-            )
-          rescue => e  
-            byebug
-          end
+          @add_expense_service.call(
+            deputy: deputy, 
+            legislature_code: deputy_expense_data["codLegislatura"], 
+            sub_quota_number: deputy_expense_data["numSubCota"], 
+            sub_quota_description: deputy_expense_data["txtDescricao"], 
+            sub_quota_specification_number: deputy_expense_data["numEspecificacaoSubCota"], 
+            sub_quota_specification_description: deputy_expense_data["txtDescricaoEspecificacao"], 
+            provider: deputy_expense_data["txtFornecedor"], 
+            provider_registration_number: deputy_expense_data["txtCNPJCPF"], 
+            document_number: deputy_expense_data["txtNumero"], 
+            document_type: analyze_document_type(document_type: deputy_expense_data["indTipoDocumento"].to_i ), 
+            issuance_date: deputy_expense_data["datEmissao"], 
+            document_value: deputy_expense_data["vlrDocumento"], 
+            reverse_value: deputy_expense_data["vlrGlosa"], 
+            net_value: deputy_expense_data["vlrLiquido"], 
+            expense_month: deputy_expense_data["numMes"], 
+            expense_year: deputy_expense_data["numAno"], 
+            installment_number: deputy_expense_data["numParcela"], 
+            passager: deputy_expense_data["txtPassageiro"], 
+            leg_trip: deputy_expense_data["txtTrecho"], 
+            batch_number: deputy_expense_data["numLote"], 
+            reimbursement_number: deputy_expense_data["numRessarcimento"], 
+            refund_value: deputy_expense_data["vlrRestituicao"], 
+            applicant_identifier: deputy_expense_data["ideDocumento"],
+            document_url: deputy_expense_data["urlDocumento"]
+          )
         end
         deputy.save!
       end
     rescue => e 
-      byebug
       raise ActiveRecord::Rollback
     end
   end
