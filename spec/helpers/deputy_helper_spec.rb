1f7392a721baae9597efc1b176d3d3b7e42a030b
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-28 11:56:11 -0300

    test: ensure DeputyHelper returns empty if expense_months is nil or empty

--- spec/helpers/deputy_helper_spec.rb
+++ spec/helpers/deputy_helper_spec.rb
@@ -66,4 +66,24 @@ describe 'DeputyHelper', type: :helper do
       end
     end
   end
+
+  context '#set_chart_labels' do
+    it 'Should return expense months' do
+      deputy_expense = create(:deputy_expense, expense_month: '3', expense_year: '2021')
+      deputy = deputy_expense.legislature.deputy
+      deputy.reload
+      expenses_by_month = deputy.expenses_for_every_month_of_year(year: '2021')
+      expect(helper.set_chart_labels(expenses_by_month: expenses_by_month)).to eq(["Março"].to_json)
+    end
+
+    context 'Should return empty' do
+      it 'when expenses_by_month is empty' do
+        expect(helper.set_chart_labels(expenses_by_month: [])).to eq([].to_json)
+      end
+
+      it 'when expenses_by_month is nil' do
+        expect(helper.set_chart_labels(expenses_by_month: nil)).to eq([].to_json)
+      end
+    end
+  end
 end
