1448a1e64a89ea7791b946a3360f46b6d96ee6ab
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-27 22:46:23 -0300

    chore: add toastr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+import './src/application.scss'
 import "bootstrap"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,6 +17,9 @@ import "bootstrap"
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 global.$ = jQuery
+import toastr from 'toastr'
+window.toastr = toastr
+
 const images = require.context('../images', true)
 
 $(window).on('load', function() {
