8ce381796d50937eeae0104e4ad90e4761ba574c
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-27 21:52:55 -0300

    feat: ensure DeputyController verifies if file was submitted

--- app/controllers/deputy_controller.rb
+++ app/controllers/deputy_controller.rb
@@ -3,6 +3,7 @@ require './app/services/deputy_management/register_deputies_expenses/analyze_exp
 require './app/services/deputy_management/create_deputy_service.rb'
 require './app/services/deputy_management/register_deputies_expenses/add_expense_service.rb'
 require './app/services/infrastructure/parse_csv_file_service.rb'
+require './app/services/deputy_management/register_deputies_expenses/create_file_control_service.rb'
 
 class DeputyController < ApplicationController
   def index
@@ -14,16 +15,24 @@ class DeputyController < ApplicationController
                                                           parse_csv_file_service: parse_csv_file_service, 
                                                           uf: "RJ"
                                                         ).call
-    Thread.new do
-      ActiveRecord::Base.transaction do
-        AnalyzeExpenseDataService.new(
-                                      expense_data: expense_data, 
-                                      create_deputy_service: CreateDeputyService.new, 
-                                      add_expense_service: AddExpenseService.new
-                                    ).call
+    redirect_to root_path, { flash: { notice: "Arquivo não contém dados!" } } and return if expense_data.empty?
+    expense_year = expense_data.values
+                               .first
+                               .first["numAno"]
+    if CreateFileControlService.new(expense_year: expense_year).call
+      Thread.new do
+        ActiveRecord::Base.transaction do
+          AnalyzeExpenseDataService.new(
+                                        expense_data: expense_data, 
+                                        create_deputy_service: CreateDeputyService.new, 
+                                        add_expense_service: AddExpenseService.new
+                                      ).call
+        end
       end
+      redirect_to deputy_rank_path, { flash: { notice: "Os dados estão sendo armazenados!" } }
+    else  
+      redirect_to root_path, { flash: { notice: "Esse arquivo já foi submetido!" } }
     end
-    redirect_to deputy_rank_path
   end
 
   def rank
