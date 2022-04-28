92537c15d4e6513c9e03807272d09c85f2b6646d
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-28 09:26:07 -0300

    refactor: create static js files to set up DataTable

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.precompile += ['node_modules/toastr/build/toastr
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( application_static.js deputy/rank_static.js deputy/details_static.js )
