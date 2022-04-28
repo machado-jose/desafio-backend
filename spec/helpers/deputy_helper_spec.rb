00920ac9bfd5a40ca2d0d2334c94e9bb2cc35c3b
Jose Gustavo <machado.josegustavo@gmail.com>
2022-04-28 11:41:37 -0300

    test: ensure DeputyHelper sets issuance date in valid format

--- spec/helpers/deputy_helper_spec.rb
+++ spec/helpers/deputy_helper_spec.rb
@@ -10,6 +10,28 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe DeputyHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'DeputyHelper', type: :helper do
+  context '#set_issuance_date' do
+    before :each do
+      @date = Time.now
+    end
+
+    it 'Should return pt format date' do
+      expect(helper.set_issuance_date(issuance_date: @date)).to eq(@date.strftime("%d/%m/%Y"))
+    end
+
+    it 'Should return en format date' do
+      expect(helper.set_issuance_date(issuance_date: @date, lang: 'en')).to eq(@date.strftime("%Y-%m-%d"))
+    end
+
+    context "Should return 'Não Informado'" do
+      it 'when date is nil' do
+        expect(helper.set_issuance_date(issuance_date: nil)).to eq('Não Informado')
+      end
+
+      it 'when language type is not informated' do
+        expect(helper.set_issuance_date(issuance_date: @date, lang: '')).to eq('Não Informado')
+      end
+    end
+  end
 end
